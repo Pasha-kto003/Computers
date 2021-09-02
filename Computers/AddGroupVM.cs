30744e2fb6e7a8bd63adbbd4a8e01395e40aa330
Паша Луковников <lukovnikov.pasha@gmail.com>
2021-09-02 20:45:30 +1000

    dwdwdw

--- Computers/AddGroupVM.cs
+++ Computers/AddGroupVM.cs
@@ -8,12 +8,31 @@ namespace Computers
 {
     public class AddGroupVM : BaseNotify
     {
-        private Group selectedGroup;
+        List<Group> Groups { get; set; }
         public int IDGroup { get; set; }
-        public int Number { get; set; }
+        public int NumberGroup { get; set; }
+        public List<Computer> Computers { get; set; }
+
+        public void AddGroup(int id, int number)
+        {
+            Group group = new Group
+            {
+                ID = 1,
+                Number = 20,
+                Computers = new List<Computer>()
+            };
+            Groups.Add(group);
+        }
+
+        public RelayCommand SaveGroup { get; set; }
+
         public AddGroupVM(MainViewModel mainVM)
         {
-
+            SaveGroup = new RelayCommand(() =>
+            {
+                AddGroup(IDGroup, NumberGroup);
+                mainVM.CurrentView = new GroupView(mainVM);
+            });
         }
     }
 }
